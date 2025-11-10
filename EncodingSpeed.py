e32eb7ef60b93eb43fe7266bd1716bd1b92048fd
Steven Slater <theunknowntardis@gmail.com>
2025-11-09 18:36:23 -0800

    Initial Commit that is very late

---
+++ EncodingSpeed.py
@@ -0,0 +1,118 @@
+# Description
+'''
+Author: Steven Slater
+Date: 2025-11-02
+Description: Output a CSV of metadata (source, destination, and encoding speed) from Handbrake logs.
+
+'''
+
+# Imports
+import cProfile
+import numpy as np
+import os
+import pandas as pd
+from pprint import pprint
+import pstats
+import re
+from re import search, findall
+import tkinter as tk
+import tkinter.filedialog as fd
+
+
+
+
+
+
+# Classes
+class Text:
+    ''''''
+    def __init__(self, body_text=''):
+        self.body_text = body_text
+        return
+
+    def show(self):
+        print(self.body_text)
+        return
+
+
+# Run
+def main():
+    basename_ = ''
+    try:
+        intro = Text('-------\nWelcome to the Handbrake Metadata Dumper!' \
+        '\nThis outputs a CSV of metadata (source, destination, and encoding speed) from Handbrake logs.')
+        intro.show()
+
+        # change this to always be wherever your Handbrake log path is; get it from the Handbrake installation settings?
+        starting_dir = r'C:\Users\Cobalt Storm\AppData\Roaming\HandBrake\logs'
+
+        root = tk.Tk()
+        root.withdraw()
+        logs = fd.askopenfilenames(parent=root, title='Select Video Files', filetypes=[('Logs', '*.txt')],
+                                   initialdir=starting_dir)
+        pprint([os.path.basename(log) for log in logs])
+
+        # get metadata
+        r_encode = r'encode_(\d{2})\.(\d{2})\.(\d{4}) (\d{2})-(\d{2})-(\d{2})\.txt'
+        r_source = r'"Path": "(.*)",'
+        r_dest = r'"File": "(.*)",'
+        r_encoder = r'"Video": {\n    "Encoder": "(.*)",'
+        r_preset = r'"Preset": "(.*)",'
+        r_RF = r'"Quality": (.*),'
+        r_speed = r'work: average encoding speed for job is (.*) fps'
+        dfs = []
+        for log in logs:
+            basename_ = os.path.basename(log)
+            file_content = ''
+            with open(log, 'r') as f:
+                file_content = f.read()
+            e_grp = search(pattern=r_encode, string=os.path.basename(log)).groups()
+            encode = f'{e_grp[2]}-{e_grp[0]}-{e_grp[1]} {e_grp[3]}:{e_grp[4]}:{e_grp[5]}'
+            source = search(pattern=r_source, string=file_content).group(1)
+            dest = search(pattern=r_dest, string=file_content).group(1)
+            encoder = search(pattern=r_encoder, string=file_content).group(1)
+            preset = search(pattern=r_preset, string=file_content).group(1)
+            qualRF = search(pattern=r_RF, string=file_content).group(1)
+            speed = search(pattern=r_speed, string=file_content).group(1)
+            meta_d = {
+                'Encode': [encode],
+                'Source': [os.path.basename(source)],       # TODO: reuse these paths to get video metadata
+                'Destination': [os.path.basename(dest)],
+                'RF': [qualRF],
+                'Preset': [preset],
+                'Speed': [int(round(float(speed), 0))],
+                'Encoder': [encoder],
+            }
+            df = pd.DataFrame(meta_d)
+            dfs.append(df)
+        if len(dfs) > 0:
+            metadata_table = pd.concat(dfs).reset_index(drop=True)
+            print(metadata_table)
+            metadata_table.to_csv(f'output_encspeed.csv')
+            print('Created CSV.')
+        else:
+            print('cancelled.')
+
+    except Exception as e:
+        print(e)
+        print(f'Filename: {basename_}')
+        raise(e)
+        #time.sleep(2)
+
+    # allow user to read message
+    print('Exited.')
+    #time.sleep(1)
+    return
+
+
+
+if __name__ == '__main__':
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+
+    main()
+
+    # profiler.disable()
+    # stats = pstats.Stats(profiler)
+    # stats.sort_stats(pstats.SortKey.CUMULATIVE)
+    # stats.print_stats(3)
---
+++ VideoMetadata.py
@@ -0,0 +1,222 @@
+# Description
+'''
+Author: Steven Slater
+Date: 2025-11-02
+Description: Output a CSV of metadata from selected video file(s).
+
+'''
+
+# Imports
+import cProfile
+import datetime
+import ffmpeg
+from fractions import Fraction
+import numpy as np
+import os
+import pandas as pd
+from pprint import pprint
+import pstats
+import time
+import tkinter as tk
+import tkinter.filedialog as fd
+
+# Classes
+class Text:
+    ''''''
+    def __init__(self, body_text=''):
+        self.body_text = body_text
+        return
+
+    def show(self):
+        print(self.body_text)
+        return
+
+class Menu:
+    ''''''
+    def __init__(self, options=None):
+        self.options = options if options is not None else {}
+        return
+    
+    def activate(self, choice: str):
+        print(f'activated {choice}')
+        return
+
+class VideosList:
+    ''''''
+    def __init__(self, videos=None):
+        self.videos = videos if videos is not None else []
+        self.metadata = MetadataTable()
+        return
+
+    def select_videos(self) -> list[str]:
+        # open file dialog with multiselect
+
+        # insert basenames of selected files into list
+        vids = []
+        return vids
+
+    def create_table(self) -> pd.DataFrame:
+        # run ffmpeg to get metadata
+
+        # choose metadata
+
+
+        table = pd.DataFrame()
+        return table
+
+class MetadataTable:
+    ''''''
+    def __init__(self, table=None):
+        self.table = pd.DataFrame(table) if table is not None else pd.DataFrame({})
+        return
+    
+    def add(self, records: dict):
+        pd.concat(self.table, pd.DataFrame(records))
+        return
+
+
+# Functions
+def get_videos(starting_dir: str, show=True):
+    ''''''
+    root = tk.Tk()
+    root.withdraw()
+    #videos = fd.askopenfilenames(parent=root, title='Select Video Files', filetypes=[('Video files', '*.mp4')])
+    videos = fd.askopenfilenames(parent=root, title='Select Video Files', initialdir=starting_dir)
+    if show:
+        pprint([os.path.basename(video) for video in videos])
+    return videos
+
+
+def get_metadata(videos: list[str]) -> list[pd.DataFrame]:
+    ''''''
+    dfs = []
+    audio_count = 0
+    for video in videos:
+        meta = ffmpeg.probe(video)
+        basename_ = os.path.basename(meta['format']['filename'])
+        date_created = datetime.datetime.fromtimestamp(os.stat(video).st_birthtime)
+        date_modified = datetime.datetime.fromtimestamp(os.stat(video).st_mtime)
+        #data_rate = os.stat(video).st_file_attributes
+        #pprint(data_rate)
+        #pprint(meta)
+
+        metav = None
+        for i, stream in enumerate(meta['streams']):
+            if stream['codec_type'] == 'audio':
+                pass
+            elif stream['codec_type'] == 'video':
+                metav = meta['streams'][i]
+                break
+            else:
+                print(f"codec_type: {stream['codec_type']}")
+
+        relevant_meta = {
+            'DateCreated': date_created,
+            'DateModified': date_modified,
+            'Name': [basename_],
+            'Extension': [os.path.splitext(meta['format']['filename'])[1][1:]],
+            'Codec': [metav['codec_name']],
+            'EncProfile': [metav['profile']],
+            'EncLevel': [int(metav['level'])],
+            'FPS': [round(float(Fraction(metav['r_frame_rate'])), 2)],     # was eval(), oof
+            'Len': [float(metav['duration'])],
+            'Res': [f"{metav['width']}x{metav['height']}"],
+            'SizeKB': [round(float(meta['format']['size']) / 1024, 3)],   # Bytes to KB
+            }
+
+        try:     # N/A for MPEG-1 and MPEG-TS
+            mpg_meta = {'Vrate': [int(metav['bit_rate'])],
+                        'Frames': [int(metav['nb_frames'])]}
+        except KeyError:
+            #print(f'MPEG-1 or MPEG-TS means video bitrate and nb_frames are N/A')
+            mpg_meta = {'Vrate': [np.nan],
+                        'Frames': [np.nan]}
+
+        # skip tracks that don't exist
+        audio_bitrates = {}
+        i = 1
+        for stream in meta['streams']:
+            if stream['codec_type'] == 'audio':
+                audio_bitrates[f'Arate{i}'] = [int(stream['bit_rate'])]
+                i += 1
+
+        # max audio streams
+        if audio_count < i:
+            audio_count = i - 1 
+            
+        rm = relevant_meta | mpg_meta | audio_bitrates
+        df = pd.DataFrame(rm)
+        dfs.append(df)
+
+    return dfs, audio_count
+
+
+def reorder_columns(columns: list[str], audio_count: int):
+    ''''''
+    print(f'audio tracks: {audio_count}')
+    for j in range(1, audio_count + 1):
+        columns += [f'Arate{j}']
+    return columns
+
+
+# Run
+def main():
+    basename_ = ''
+    try:
+        intro = Text('-------\nWelcome to the Video Metadata Dumper!' \
+        '\nThis outputs the chosen metadata of selected video files into a CSV.')
+        intro.show()
+
+        starting_dir = r'A:\Videos\Editing Exports\Handbrake'
+        videos = get_videos(starting_dir=starting_dir)
+
+        dfs, audio_count = get_metadata(videos=videos)
+
+        if len(dfs):
+            metadata_table = pd.concat(dfs).reset_index(drop=True)
+            columns = ['DateCreated', 
+                       'DateModified', 
+                       'Name', 
+                       'Extension', 
+                       'Codec', 
+                       'EncProfile', 
+                       'EncLevel',
+                       'FPS', 
+                       'Len', 
+                       'Frames', 
+                       'Res', 
+                       'SizeKB', 
+                       'Vrate']
+            cols = reorder_columns(columns=columns, audio_count=audio_count)
+            pprint(cols)
+            mtable = metadata_table[cols]
+            pprint(mtable)
+            mtable.to_csv(f'output.csv')
+            print('Created CSV.')
+        else:
+            print('cancelled.')
+
+    except Exception as e:
+        print(e)
+        print(f'Filename: {basename_}')
+        raise(e)
+        #time.sleep(2)
+
+    # allow user to read message
+    print('Exited.')
+    #time.sleep(1)
+    return
+
+
+
+
+if __name__ == '__main__':
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+
+    main()
+
+    # profiler.disable()
+    # stats = pstats.Stats(profiler)
+    # stats.sort_stats(pstats.SortKey.CUMULATIVE)
+    # stats.print_stats(3)
